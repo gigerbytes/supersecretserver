233ccec4274aea6b739080d5e4e54be13fa30eb1
Björn Schmidtke <bs@pa1nd.de>
2017-03-18 13:33:40 +0100

    wip

--- routes.js
+++ routes.js
@@ -4,35 +4,27 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 
 var router = module.exports = express.Router();
 
-// controllers
 var messages = require("./controllers/messages");
 
+// home
 router.get('/', messages.index);
 
+// messages
 router.get('/messages/new', messages.new);
 router.post('/messages/create', messages.create);
 router.get('/messages/show/:id', messages.show);
 router.get('/messages/list', messages.list);
 router.get('/messages/image/:id', messages.image);
 
-// router.get('/user', function (req, res) {
-//   res.send('URE IN!!');
-// });
 
 // ############################
-// Auth0
+// Auth0 - users
 // Render the login template
 router.get('/login',
   function(req, res){
     console.log(callbackURL)
     res.render('login', { env: process.env, callbackURL: callbackURL });
   });
-
-// Perform session logout and redirect to homepage
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
 
 // Perform the final stage of authentication and redirect to '/user'
 router.get('/callback',
@@ -41,6 +33,12 @@ router.get('/callback',
     console.log('authenticated')
     res.redirect(req.session.returnTo || '/user');
   });
+
+// Perform session logout and redirect to homepage
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 // Get the user profile
 router.get('/user', ensureLoggedIn, function(req, res, next) {
